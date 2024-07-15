cad4e3facde137ce300e8462e37e1c2f54ea8c82
ezun.kim <ezun.kim@samsung.com>
2024-07-15 12:49:01 +0900

    dist

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
